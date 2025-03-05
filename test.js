0cd9607b6945f67338743b0ea64d6a745cb0942c
forz-man <abforson01@gmail.com>
2025-03-05 16:19:01 +0100

    test failed

--- test.js
+++ test.js
@@ -14,3 +14,7 @@ test("multiple numbers  seperated by comma returns thier sum", () => {
   expect(add("1,2,3")).toBe(6);
   expect(add("1,5,7")).toBe(13);
 } );
+test("numbers separated by new line and commas return thier sum ", () => {
+  expect(add("1\n2,3")).toBe(6);
+  expect(add("1\n5,7")).toBe(13);
+} );
