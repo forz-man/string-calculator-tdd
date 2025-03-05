04ff967279b90c4fa7bc1f509d6623a16430847d
forz-man <abforson01@gmail.com>
2025-03-05 15:56:06 +0100

    expected error for 2 numbers seperated by comma

--- test.js
+++ test.js
@@ -2,3 +2,12 @@ const add = require ("./function.js");
 test("empty string returns 0", () => {
   expect(add("")).toBe(0);
 } );
+test("single number returns the number", () => {
+  expect(add("1")).toBe(1); 
+  expect(add("5")).toBe(5);
+} );
+test("two numbers depersted by comma returns thier sum", () => {
+  expect(add("1,2")).toBe(3);
+  expect(add("1,5")).toBe(6);
+} );
+
